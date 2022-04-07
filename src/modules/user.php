21d4fb67b9f820cf6ee3171b8809db4f0afea4ac
ErikMyllymaki <erik.myl01@gmail.com>
2022-04-07 12:01:47 +0300

    getUsers()

--- src/modules/user.php
+++ src/modules/user.php
@@ -1,1 +1,15 @@
 <?php
+
+function getUsers() {
+    require_once MODULES_DIR.'db.php';
+
+    try {
+        $pdo = getPdoConnection();
+        $sql = "SELECT * FROM kayttaja";
+        $people = $pdo->query($sql);
+
+        return $people->fetchAll();
+    }catch(PDOException $e){
+        throw $e;
+    }
+}
