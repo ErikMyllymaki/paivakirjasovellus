edaa2205dbfa5d28eef678bde3044f45c04062f3
Jennivis <n1vije00@students.oamk.fi>
2022-04-07 12:23:38 +0300

    Lisätty avainsanoja

--- public/diaryentry.php
+++ public/diaryentry.php
@@ -5,6 +5,13 @@ include TEMPLATES_DIR.'header.php';
     <form action="diaryentry.php" method="post">
         <h3>Kirjoita merkintä</h3>
         <textarea id="diary" name="diary"></textarea><br/>
+        <h4>Valitse avainsanat</h4>
+        <input type="checkbox" id="fun" name="fun"
+         checked>
+         <label for="fun">Hauskaa</label>
+         <input type="checkbox" id="boring" name="boring"
+         checked>
+         <label for="boring">Tylsää</label><br/>
         <input type="submit" class="btn btn-primary" value="Lisää merkintä">
     </form>
 
--- public/index.php
+++ public/index.php
@@ -2,4 +2,5 @@
 include TEMPLATES_DIR.'header.php';
 
 
+
 include TEMPLATES_DIR.'footer.php';
