4e45a12a2141acd0a3bd7f017b379100977c99ec
Eveliina Kaarivaara <eveliina.kaarivaara@gmail.com>
2022-04-07 11:59:30 +0300

    lisätty form

--- public/user.php
+++ public/user.php
@@ -1,5 +1,23 @@
 <?php
     include TEMPLATES_DIR."header.php";
+?>
 
+<form action="user.php" method="post">
+        <label for="fname">Etunimi:</label><br>
+        <input type="text" name="fname" id="fname"><br>
+
+        <label for="lname">Sukunimi:</label><br>
+        <input type="text" name="lname" id="lname"><br>
+
+        <label for="uname">Käyttäjänimi:</label><br>
+        <input type="text" name="uname" id="uname"><br>
+
+        <label for="pw">Salasana:</label><br>
+        <input type="password" name="pw" id="pw"><br>
+
+        <input type="submit" class="btn btn-primary" value="Rekisteröidy">
+</form>
+
+<?php
     include TEMPLATES_DIR.'footer.php';
 ?>
