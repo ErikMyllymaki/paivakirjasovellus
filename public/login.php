799cd73c0a63aa24c09ddf26058980e466acfc73
Jennivis <n1vije00@students.oamk.fi>
2022-04-07 11:47:58 +0300

    Lisätty sisäänkirjautumiseen lomake

--- public/login.php
+++ public/login.php
@@ -1,5 +1,14 @@
 <?php
     include TEMPLATES_DIR."header.php";
-
-    include TEMPLATES_DIR.'footer.php';
 ?>
+
+    <form action="login.php" method="post">
+        <h3>Kirjaudu sisään</h3>
+        <label for="username">Käyttäjänimi:</label><br>
+        <input type="text" name="username" id="username"><br>
+        <label for="password">Salasana:</label><br>
+        <input type="password" name="password" id="password"><br>
+        <input type="submit" class="btn btn-primary" value="Kirjaudu">
+    </form>
+
+ <?php  include TEMPLATES_DIR.'footer.php'; ?>
