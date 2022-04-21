d4564ef10635d6369365aa2afcaa81b41228b97b
Eveliina Kaarivaara <eveliina.kaarivaara@gmail.com>
2022-04-21 12:59:50 +0300

    poistettu ylimääränen footer

--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
 <?php
 include TEMPLATES_DIR.'header.php';
-include TEMPLATES_DIR.'footer.php';
-
 
 if(isset($_SESSION["username"])){
         echo "<h1>Welcome ".$_SESSION["fname"]." ".$_SESSION["lname"]."</h1>";
@@ -9,4 +7,4 @@ if(isset($_SESSION["username"])){
         echo "<h1>Welcome! You may log in to use advanced features!</h1>";
     }
 
-    include TEMPLATES_DIR.'footer.php';
+include TEMPLATES_DIR.'footer.php';
