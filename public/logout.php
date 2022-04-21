fc75baf786303617f4afef3fb1046136fa13f3cf
Jennivis <n1vije00@students.oamk.fi>
2022-04-21 10:47:58 +0300

    Lisätty uloskirjautuminen

--- public/logout.php
+++ public/logout.php
@@ -1,5 +1,13 @@
 <?php
     include TEMPLATES_DIR.'header.php';
+    include MODULES_DIR.'authorization.php';
+
+    if(isset($_SESSION["uname"])){
+        logout();
+        header("Location: logout.php");
+    }else{
+        echo '<div class="alert alert-success" role="alert">Olet kirjautunut ulos.</div>';
+    }
 
     include TEMPLATES_DIR.'footer.php.';
 ?>
--- src/modules/authorization.php
+++ src/modules/authorization.php
@@ -1,1 +1,12 @@
 <?php
+
+
+function logout(){
+
+    try{
+        session_unset();
+        session_destroy();
+    }catch(Exception $e){
+        throw $e;
+    }
+}
