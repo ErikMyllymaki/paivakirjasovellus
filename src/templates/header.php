469c2a5decc0980afd570b31dc0d94cc1674bfc5
Jenni Lohi <jennilohi@hotmail.com>
2022-04-22 12:05:15 +0300

    kommentteja lisätty

--- src/templates/header.php
+++ src/templates/header.php
@@ -10,8 +10,9 @@ session_start();
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+    <!-- tyylitiedosto -->
     <?php
-        include CSS_DIR.'style.php';
+       include CSS_DIR.'style.php';
     ?>
 
     <title>Päiväkirja</title>
