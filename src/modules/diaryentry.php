65e9a282405f8b49ee5f741c154eade271945358
Jenni Lohi <jennilohi@hotmail.com>
2022-04-22 12:35:25 +0300

    getdiaryentries -funktio tehty

--- src/modules/diaryentry.php
+++ src/modules/diaryentry.php
@@ -16,3 +16,23 @@ function addDiaryEntry($kayttaja_id, $merkinta){
         throw $e;
     }
 }
+
+
+function getDiaryEntries() {
+    require_once MODULES_DIR.'db.php';
+
+    try {
+        $pdo = getPdoConnection();
+
+        $sql = "SELECT merkinta, aika, kayttajanimi 
+            FROM pk_merkinta INNER JOIN kayttaja ON 
+            pk_merkinta.kayttaja_id=kayttaja.kayttaja.id
+            ORDER BY kayttajanimi, aika;";
+
+        $diaryentry = $pdo->query($sql);
+
+        return $diaryentry->fetchAll();
+    } catch(PDOException $e) {
+        throw $e;
+    }
+}
