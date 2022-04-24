816573ca76658265c7e03c0854b853de71445cb0
Jennivis <n1vije00@students.oamk.fi>
2022-04-24 21:19:14 +0300

    Muutettu tekstejä ja otsikoita. Lisätty muutama css juttu.

--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
 include TEMPLATES_DIR.'header.php';
 
 if(isset($_SESSION["username"])){
-        echo "<h1>Welcome ".$_SESSION["fname"]." ".$_SESSION["lname"].$_SESSION["user_id"]."</h1>";
+        echo "<h1>Tervetuloa ".$_SESSION["fname"]." ".$_SESSION["lname"].$_SESSION["user_id"]."</h1>";
     }else{
-        echo "<h1>Welcome! You may log in to use advanced features!</h1>";
+        echo "<h1>Tervetuloa. Kirjaudu sisään tehdäksesi päiväkirjamerkintöjä. Rekisteröidy ensin, jos et ole vielä rekisteröitynyt.</h1>";
     }
 
 include TEMPLATES_DIR.'footer.php';
