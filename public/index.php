5dd4c93efa17c6141444bdb3b47d5b120695de1e
Eveliina Kaarivaara <eveliina.kaarivaara@gmail.com>
2022-05-06 18:16:29 +0300

    muokattu etusivun tekstiä uloskirjautuneena

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ include TEMPLATES_DIR.'header.php';
 if(isset($_SESSION["username"])){?>
         <div id="box"> <?php echo "<h1>Tervetuloa ".$_SESSION["fname"]." ".$_SESSION["lname"]."</h1>";
         echo "<p>Voit nyt lisätä merkintöjä päiväkirjaasi tai tarkastella aiempia merkintöjäsi.</p>"; ?> </div> <?php
-    }else{
-        echo "<h1>Tervetuloa. Kirjaudu sisään tehdäksesi päiväkirjamerkintöjä. Rekisteröidy ensin, jos et ole vielä rekisteröitynyt.</h1>";
+    }else{?>
+        <div id="box"> <?php echo "<h1>Tervetuloa. Kirjaudu sisään tehdäksesi päiväkirjamerkintöjä. Rekisteröidy ensin, jos et ole vielä rekisteröitynyt.</h1>"; ?> </div> <?php
     }
 
 include TEMPLATES_DIR.'footer.php';
