3c3d3d30fb66f928cb074530214a700f5a68cfd8
Jennivis <n1vije00@students.oamk.fi>
2022-05-06 12:58:38 +0300

    Etusivun tekstin asettelua muutettu

--- public/index.php
+++ public/index.php
@@ -1,8 +1,9 @@
 <?php
 include TEMPLATES_DIR.'header.php';
 
-if(isset($_SESSION["username"])){
-        echo "<h1>Tervetuloa ".$_SESSION["fname"]." ".$_SESSION["lname"].$_SESSION["user_id"]."</h1>";
+if(isset($_SESSION["username"])){?>
+        <div id="box"> <?php echo "<h1>Tervetuloa ".$_SESSION["fname"]." ".$_SESSION["lname"]." ".$_SESSION["user_id"]."</h1>";
+        echo "<p>Voit nyt lisätä merkintöjä päiväkirjaasi tai tarkastella aiempia merkintöjäsi.</p>"; ?> </div> <?php
     }else{
         echo "<h1>Tervetuloa. Kirjaudu sisään tehdäksesi päiväkirjamerkintöjä. Rekisteröidy ensin, jos et ole vielä rekisteröitynyt.</h1>";
     }
