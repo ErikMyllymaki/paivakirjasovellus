ddf419352fbeca88b37225d45873de716c94a244
ErikMyllymaki <erik.myl01@gmail.com>
2022-05-06 14:41:33 +0300

    radio buttonit toimii jotenkin

--- public/diaryentries.php
+++ public/diaryentries.php
@@ -1,19 +1,27 @@
 <?php
 include TEMPLATES_DIR."header.php";
 include MODULES_DIR."diaryentry.php";
+include MODULES_DIR."diaryentries.php";
+
 
 
 
 $id = filter_input(INPUT_GET, "id");
 
-if(isset($id)){
-    try {
-        deleteDiaryEntry($id);
-        echo '<div class="alert alert-success" role="alert">Merkintä poistettu!!</div>';
-    } catch (Exception $e) {
-        throw $e;
-    }
-}
+// if(isset($id)){
+//     try {
+//         deleteDiaryEntry($id);
+//         echo '<div class="alert alert-success" role="alert">Merkintä poistettu!!</div>';
+//     } catch (Exception $e) {
+//         throw $e;
+//     }
+// }
+
+
+
+
+
+
 
 ?>
 
@@ -23,21 +31,56 @@ if(isset($id)){
 
 <?php
 
-$diaryEntries = getDiaryEntries();
-$keywords = getKeyWords();
 
+$keywords = getKeyWords();
 echo '<h2>Valitse avainsanat:</h2>';
 
-foreach($keywords as $keyword) {
-    echo '<input type="checkbox" name="check[]" value="'.$keyword['avainsana_id'].'">
-    <label for "'.$keyword['nimi'].'">'. $keyword['nimi'] .'</label>';
+?>
+
+<form method="post" action="diaryentries.php">
+<?php
+// echo '<input type="radio" checked name="check" value="'.$keywords[0]['avainsana_id'].'">
+// <label for "'.$keywords[0]['nimi'].'">'. $keywords[0]['nimi'] .'</label>';
+for($x = 0; $x < sizeof($keywords); $x++) {
+    echo '<input type="radio" name="check" id="" value="'.$keywords[$x]['avainsana_id'].'">
+    <label for "'.$keywords[$x]['nimi'].'">'. $keywords[$x]['nimi'] .'</label>';
+} 
+
+
+
+
+?>
+<input type="submit" value="Submit" name="submit">
+</form>
+
+<?php
+try {
+       if(isset($_POST['submit'])) {
+        if(!empty($_POST['check']))
+        $diaryEntries = getDiaryEntries($_POST['check']);
+   
+    foreach($diaryEntries as $diaryEntry) {
+        echo "<h2>".$diaryEntry["merkinta"]."</h2><h3>".$diaryEntry["merkinta_id"].' id'.$diaryEntry['avainsana_id'];
+    }
+}
+} catch (Exception $e) {
+    echo 'Hae jollain avainsanalla!';
 }
 
-foreach($diaryEntries as $diaryEntry) {
-    echo "<h2>".$diaryEntry["aika"]."</h2><h3>".$diaryEntry["kayttajanimi"]."</h3><p>".$diaryEntry["merkinta"].'<a href="diaryentries.php?id=' . $diaryEntry["merkinta_id"] . '" class="btn btn-primary">Poista</a> </li>'. "</p>" . $diaryEntry["nimi"] . "<br></br>" ;/* mapataanko kaikki avainsananimet yhteen kirjaukseen? */
-}
+// foreach($diaryEntries as $diaryEntry) {
+//     echo "<h2>".$diaryEntry["merkinta"]."</h2><h3>".$diaryEntry["merkinta_id"]."</h3><p>".$diaryEntry["merkinta"].'<a href="diaryentries.php?id=' . $diaryEntry["merkinta_id"] . '" class="btn btn-primary">Poista</a> </li>'. "</p>" . $diaryEntry["nimi"] . "<br></br>" ;/* mapataanko kaikki avainsananimet yhteen kirjaukseen? */
+// }
 
 
 
+
+?>
+
+
+
+    
+
+
+<?php
 include TEMPLATES_DIR.'footer.php';
 ?>
--- src/modules/diaryentries.php
+++ src/modules/diaryentries.php
@@ -1,23 +1,20 @@
 
 <?php
 
-function getDiaryEntries() {
+function getDiaryEntries($id) {
     require_once MODULES_DIR.'db.php';
 
     try {
         $pdo = getPdoConnection();
 
-        $sql = "SELECT pk_merkinta.merkinta_id, merkinta from pk_merkinta, avainsanarivi
+        $sql = ("SELECT avainsanarivi.avainsana_id, pk_merkinta.merkinta_id, merkinta from pk_merkinta, avainsanarivi
         where (pk_merkinta.merkinta_id = avainsanarivi.merkinta_id)
-        and avainsanarivi.avainsana_id in (1,2,3)";
+        and avainsanarivi.avainsana_id = $id");
 
-        $diaryentry = $pdo->query($sql);
+        $diaryentries = $pdo->query($sql);
 
-        $diaryentries = $diaryentry->fetchAll();
+        return $diaryentries->fetchAll();
 
-        foreach ($diaryentries as $diaryentry) {
-           return echo $diaryentry['merkinta_id'].$diaryentry['merkinta_nimi'];
-        }
     } catch(PDOException $e) {
         throw $e;
     }
